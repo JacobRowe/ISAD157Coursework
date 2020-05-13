f79a6571b41992d80a2a526a6711e791c6481c48
JacobRowe <55706787+JacobRowe@users.noreply.github.com>
2020-05-13 12:49:47 +0100

    ALL THE CODE
    All code in git package

---
+++ ISAD157StartCode/ISAD157/SOFT562Week18/DatabaseForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SOFT562Week18
+{
+    partial class DatabaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.dataGridViewOther = new System.Windows.Forms.DataGridView();
+            this.comboBoxQueryChoice = new System.Windows.Forms.ComboBox();
+            this.dataGridViewFreindInfo = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataLabelUser = new System.Windows.Forms.TextBox();
+            this.dataLabelInfo = new System.Windows.Forms.TextBox();
+            this.dataLabelFreinds = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOther)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFreindInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.AllowUserToAddRows = false;
+            this.dataGridViewUsers.AllowUserToDeleteRows = false;
+            this.dataGridViewUsers.AllowUserToOrderColumns = true;
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(17, 38);
+            this.dataGridViewUsers.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.RowHeadersWidth = 51;
+            this.dataGridViewUsers.RowTemplate.Height = 24;
+            this.dataGridViewUsers.Size = new System.Drawing.Size(579, 585);
+            this.dataGridViewUsers.TabIndex = 0;
+            // 
+            // dataGridViewOther
+            // 
+            this.dataGridViewOther.AllowUserToAddRows = false;
+            this.dataGridViewOther.AllowUserToDeleteRows = false;
+            this.dataGridViewOther.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOther.Location = new System.Drawing.Point(609, 38);
+            this.dataGridViewOther.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewOther.Name = "dataGridViewOther";
+            this.dataGridViewOther.RowHeadersWidth = 51;
+            this.dataGridViewOther.RowTemplate.Height = 24;
+            this.dataGridViewOther.Size = new System.Drawing.Size(463, 585);
+            this.dataGridViewOther.TabIndex = 1;
+            // 
+            // comboBoxQueryChoice
+            // 
+            this.comboBoxQueryChoice.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxQueryChoice.FormattingEnabled = true;
+            this.comboBoxQueryChoice.Items.AddRange(new object[] {
+            "Freindships",
+            "WorkPlace",
+            "School",
+            "Messages"});
+            this.comboBoxQueryChoice.Location = new System.Drawing.Point(253, 628);
+            this.comboBoxQueryChoice.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxQueryChoice.Name = "comboBoxQueryChoice";
+            this.comboBoxQueryChoice.Size = new System.Drawing.Size(569, 29);
+            this.comboBoxQueryChoice.TabIndex = 2;
+            this.comboBoxQueryChoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxQueryChoice_SelectedIndexChanged);
+            // 
+            // dataGridViewFreindInfo
+            // 
+            this.dataGridViewFreindInfo.AllowUserToAddRows = false;
+            this.dataGridViewFreindInfo.AllowUserToDeleteRows = false;
+            this.dataGridViewFreindInfo.AllowUserToOrderColumns = true;
+            this.dataGridViewFreindInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFreindInfo.Location = new System.Drawing.Point(1070, 38);
+            this.dataGridViewFreindInfo.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewFreindInfo.Name = "dataGridViewFreindInfo";
+            this.dataGridViewFreindInfo.RowHeadersWidth = 51;
+            this.dataGridViewFreindInfo.RowTemplate.Height = 24;
+            this.dataGridViewFreindInfo.Size = new System.Drawing.Size(311, 585);
+            this.dataGridViewFreindInfo.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(840, 634);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Change user, and select info again to update datagrids";
+            // 
+            // dataLabelUser
+            // 
+            this.dataLabelUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataLabelUser.Location = new System.Drawing.Point(17, 8);
+            this.dataLabelUser.Name = "dataLabelUser";
+            this.dataLabelUser.Size = new System.Drawing.Size(68, 27);
+            this.dataLabelUser.TabIndex = 5;
+            this.dataLabelUser.Text = "USERS";
+            // 
+            // dataLabelInfo
+            // 
+            this.dataLabelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataLabelInfo.Location = new System.Drawing.Point(609, 8);
+            this.dataLabelInfo.Name = "dataLabelInfo";
+            this.dataLabelInfo.Size = new System.Drawing.Size(50, 27);
+            this.dataLabelInfo.TabIndex = 6;
+            this.dataLabelInfo.Text = "INFO";
+            // 
+            // dataLabelFreinds
+            // 
+            this.dataLabelFreinds.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataLabelFreinds.Location = new System.Drawing.Point(1070, 8);
+            this.dataLabelFreinds.Name = "dataLabelFreinds";
+            this.dataLabelFreinds.Size = new System.Drawing.Size(86, 27);
+            this.dataLabelFreinds.TabIndex = 7;
+            this.dataLabelFreinds.Text = "FREINDS";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(153, 631);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(95, 23);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "INFO SELECT";
+            // 
+            // DatabaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1400, 660);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataLabelFreinds);
+            this.Controls.Add(this.dataLabelInfo);
+            this.Controls.Add(this.dataLabelUser);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridViewFreindInfo);
+            this.Controls.Add(this.comboBoxQueryChoice);
+            this.Controls.Add(this.dataGridViewOther);
+            this.Controls.Add(this.dataGridViewUsers);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "DatabaseForm";
+            this.Text = "Facebook User Data Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOther)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFreindInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.DataGridView dataGridViewOther;
+        private System.Windows.Forms.ComboBox comboBoxQueryChoice;
+        private System.Windows.Forms.DataGridView dataGridViewFreindInfo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox dataLabelUser;
+        private System.Windows.Forms.TextBox dataLabelInfo;
+        private System.Windows.Forms.TextBox dataLabelFreinds;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ISAD157StartCode/ISAD157/SOFT562Week18/DatabaseForm.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SOFT562Week18
+{
+    public partial class DatabaseForm : Form
+    {
+        //Runs first
+        public DatabaseForm()
+        {
+            InitializeComponent();
+
+            //Set first data grid to fill with all users at start of program
+
+            //String containing connection info specified in DBconnect.cs
+            string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+            //Create a connection to the MySQL Server database
+            using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+            {
+                //Query to retrieve all the records in the users table
+                string query = "SELECT * FROM isad157_jrowe.users";
+
+                //Opens a database connection with the property settings from ConnectionString
+                connection.Open();
+
+                //Submit the SQL statement, in the form of the string query and the connection, to be executed
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                // The MySqlDataAdapter represents a set of data commands
+                // and a database connection that are used to fill a
+                // dataset and update a MySQL database.
+                MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                //Creates new data table called userDataTable for binding to data grid later
+                DataTable userDataTable = new DataTable();
+                //Fills the MySqlDataAdapter sqlDA with the data table freindDataTable
+                sqlDA.Fill(userDataTable);
+
+                // Bind the user table to the Data Grid View users
+                dataGridViewUsers.DataSource = userDataTable;
+
+            } //End of connection to MySql Server Database
+        }
+
+        //Runs when the selected index of the drop down menu comboBoxQueryChoice changes
+        private void comboBoxQueryChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxQueryChoice.SelectedIndex == 0)
+            {
+                //String containing connection info specified in DBconnect.cs
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                //Create a connection to the MySQL Server database
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    //value for the currently selected index of the row, plus one so the user id matches with the value
+                    int currentRow = dataGridViewUsers.CurrentCell.RowIndex + 1;
+
+                    //Query to retrieve all the records in the freindships table where the user id or the reciver user id 
+                    //is the currently selected row index, aka the selected user in the data grid
+                    string query1 = "SELECT * FROM isad157_jrowe.freindships WHERE user_id = " + currentRow;
+                    //Opens a database connection with the property settings from ConnectionString
+                    connection.Open();
+
+                    //Submit the SQL statement, in the form of the string query and the connection, to be executed
+                    MySqlCommand cmd = new MySqlCommand(query1, connection);
+
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    //Creates new data table called freindDataTable for binding to data grid later
+                    DataTable freindDataTable = new DataTable();
+                    //Fills the MySqlDataAdapter sqlDA with the data table freindDataTable
+                    sqlDA.Fill(freindDataTable);
+
+                    //Attatches the freind data table to the Data Grid View other.
+                    dataGridViewOther.DataSource = freindDataTable;
+
+                    //Query to retrive user_id, first_name and last_name from the users table where the user_id 
+                    //is equal to the friend_id from freindships table where the user_id is equal to the current selected user
+                    //
+                    //AKA, shows freind info contextual to what user has been selected and who they are freinds with
+                    string query2 = "SELECT user_id, first_name, last_name FROM isad157_jrowe.users WHERE user_id = ANY (SELECT friend_id FROM isad157_jrowe.freindships WHERE user_id = " + currentRow + ")";
+
+                    //Submit the SQL statement, in the form of the string query and the connection, to be executed
+                    MySqlCommand cmd2 = new MySqlCommand(query2, connection);
+
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database
+                    MySqlDataAdapter sqlDA2 = new MySqlDataAdapter(cmd2);
+                    //Creates new data table called freindInfoDataTable for binding to data grid later
+                    DataTable freindInfoDataTable = new DataTable();
+                    //Fills the MySqlDataAdapter sqlDA with the data table freindInfoDataTable
+                    sqlDA2.Fill(freindInfoDataTable);
+
+                    //Attatches the freindInfo data table to the Data Grid View freindInfo.
+                    dataGridViewFreindInfo.DataSource = freindInfoDataTable;
+                }//End of connection to MySql Server Database
+            }
+
+            else if (comboBoxQueryChoice.SelectedIndex == 1)
+            {
+                //String containing connection info specified in DBconnect.cs
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                //Create a connection to the MySQL Server database
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    //value for the currently selected index of the row, plus one so the user id matches with the value
+                    int currentRow = dataGridViewUsers.CurrentCell.RowIndex + 1;
+
+                    //Query to retrieve all the records in the workplace table where the user id or the reciver user id 
+                    //is the currently selected row index, aka the selected user in the data grid
+                    string query = "SELECT * FROM isad157_jrowe.workplace WHERE user_id = " + currentRow;
+
+                    //Opens a database connection with the property settings from ConnectionString
+                    connection.Open();
+
+                    //Submit the SQL statement, in the form of the string query and the connection, to be executed
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    //Creates new data table called workPlaceDataTable for binding to data grid later
+                    DataTable workPlaceDataTable = new DataTable();
+                    //Fills the MySqlDataAdapter sqlDA with the data table workPlaceDataTable
+                    sqlDA.Fill(workPlaceDataTable);
+
+                    //Attatches the workplace data table to the Data Grid View other.
+                    dataGridViewOther.DataSource = workPlaceDataTable;
+                }//End of connection to MySql Server Database
+            }
+
+            else if (comboBoxQueryChoice.SelectedIndex == 2)
+            {
+                //String containing connection info specified in DBconnect.cs
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                //Create a connection to the MySQL Server database
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    //value for the currently selected index of the row, plus one so the user id matches with the value
+                    int currentRow = dataGridViewUsers.CurrentCell.RowIndex + 1;
+
+                    //Query to retrieve all the records in the school table where the user id or the reciver user id 
+                    //is the currently selected row index, aka the selected user in the data grid
+                    string query = "SELECT * FROM isad157_jrowe.school WHERE user_id = " + currentRow;
+
+                    //Opens a database connection with the property settings from ConnectionString
+                    connection.Open();
+
+                    //Submit the SQL statement, in the form of the string query and the connection, to be executed
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    //Creates new data table called schoolDataTable for binding to data grid later
+                    DataTable schoolDataTable = new DataTable();
+                    //Fills the MySqlDataAdapter sqlDA with the data table schoolDataTable
+                    sqlDA.Fill(schoolDataTable);
+
+                    //Attatches the school data table to the Data Grid View other.
+                    dataGridViewOther.DataSource = schoolDataTable;
+                }//End of connection to MySql Server Database
+            }
+
+            else if (comboBoxQueryChoice.SelectedIndex == 3)
+            {
+                //String containing connection info specified in DBconnect.cs
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                    "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                    DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                    DBConnect.PASSWORD + ";" + "SslMode=" +
+                    DBConnect.SslMode + ";";
+
+                //Create a connection to the MySQL Server database
+                using (MySqlConnection connection =
+                    new MySqlConnection(connectionString))
+                {
+                    //value for the currently selected index of the row, plus one so the user id matches with the value
+                    int currentRow = dataGridViewUsers.CurrentCell.RowIndex + 1;
+
+                    //Query to retrieve all the records in the messages table where the user id or the reciver user id 
+                    //is the currently selected row index, aka the selected user in the data grid
+                    string query = "SELECT * FROM isad157_jrowe.messages WHERE user_id = " + currentRow + " OR  reciver_user_id = " + currentRow;
+
+                    //Opens a database connection with the property settings from ConnectionString
+                    connection.Open();
+
+                    //Submit the SQL statement, in the form of the string query and the connection, to be executed
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    // The MySqlDataAdapter represents a set of data commands
+                    // and a database connection that are used to fill a
+                    // dataset and update a MySQL database
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    //Creates new data table called messagesDataTable for binding to data grid later
+                    DataTable messagesDataTable = new DataTable();
+                    //Fills the MySqlDataAdapter sqlDA with the data table messagesDataTable
+                    sqlDA.Fill(messagesDataTable);
+
+                    //Attatches the messages data table to the Data Grid View other.
+                    dataGridViewOther.DataSource = messagesDataTable;
+                }//End of connection to MySql Server Database
+            }
+        }
+    }
+}
+
